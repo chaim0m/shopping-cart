75ecaa414c06a1e586a86913f0876c3e6514d91e
Chaim Finson <chaim0m@hotmail.com>
2018-01-04 11:02:13 +0200

    finally works

--- public/script.js
+++ public/script.js
@@ -1,19 +1,19 @@
 var cart = []; // an array with all of our cart items
-var idgenerator = 0;
+// var idgenerator = 0;
 var total = 0;
-var freq = 1;
-var isIn = true;
+// var freq = 1;
+// var isIn = true;
 // var freq = $(this).closest('.card.item').data().freq;
 var source = $('#cartinfo').html();
 var template = Handlebars.compile(source);
 var updateCart = function() {
   $('.cart-list').contents().remove();
   $('.total').contents().remove();
-
+  total = 0;
   for (let i = 0; i < cart.length; i++) {
     var newHTML = template(cart[i]);
-    total += cart[i].price;
     $('.cart-list').append(newHTML);
+    total += cart[i].freq * cart[i].price;
 
   }
   $('.total').append(total);
@@ -22,40 +22,57 @@ var updateCart = function() {
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
 }
+// var total = function() {
+//   let sum = 0;
+//   for (let i = 0; i < length.cart; i++) {
+//     sum += cart[i].freq * cart[i].price;
+//   }
+//   return sum
+// }
+// function findItem(itemName) {
+//   for (let i = 0; i < cart.length; i++) {
+//     if (itemName == cart[i].name) {
+//       return cart[i];
+//     }
+//   }
+//   return null;
 
-function findItem(itemName) {
-  for (let i = 0; i < cart.length; i++) {
-    if (itemName == cart[i].name) {
-      return cart[i];
-    }
-  }
-  return null;
+// return -1 if the item is not in the list
+// return i
+// }
+// var addItem = function(name,id,price) {
+//   var item = findItem(name);
+//
+//   if (item == null) {
+//     var newItem = {
+//       id: id,
+//       name: name,
+//       price: price
+//     };
+//     cart.push(newItem);
+//     newItem.freq = freq;
+//     totalArray.push(item.price);
+//   } else {
+//       item.freq++;
+//       totalArray.push(item.price);
+//   }
+//
+// }
 
-  // return -1 if the item is not in the list
-  // return i
-}
-var addItem = function(name,id,price) {
-  var item = findItem(name);
-
-  if (item == null) {
-    var newItem = {
-      id: id,
-      name: name,
-      price: price
-    };
-    cart.push(newItem);
-    newItem.freq = freq;
-  } else {
-      item.freq++;
-  }
-
-}
-}
 // adds item AND checks frequency of items inserting it into key 'freq' of item object.
 
-
+var addItem = function(item) {
+  for (var i = 0; i < cart.length; i++) {
+    if (item.name === cart[i].name) {
+      cart[i].freq += 1;
+      return;
+    }
+  }
+  cart.push(item)
+}
 var clearCart = function() {
   cart = [];
+  // totalArray = [];
   $('.cart-list').contents().remove();
   $('.total').contents().remove();
   $('.total').text('0');
@@ -70,7 +87,12 @@ $('.view-cart').on('click', function() {
 $('.add-to-cart').on('click', function() {
   var name = $(this).closest('.card.item').data().name;
   var price = $(this).closest('.card.item').data().price;
-  var id = idgenerator++;
+  // var id = idgenerator++;
+  var item = {
+    name: name,
+    price: price,
+    freq: 1
+  };
 
 
   // TODO: get the "item" object from the page
